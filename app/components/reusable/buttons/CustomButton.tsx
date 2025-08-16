67cf8a91a19879a52e708737f6b556d7496553ee
mabdelkarimbc <mabdelkarim@beyond-creation.net>
2025-08-16 17:19:30 +0300

    added disbaled prop to button

--- app/components/reusable/buttons/CustomButton.tsx
+++ app/components/reusable/buttons/CustomButton.tsx
@@ -1,29 +1,33 @@
 "use client";
 
 interface CustomButtonProps {
-  title: string;
-  action?: () => void;
-  url?: string;
+	title: string;
+	action?: () => void;
+	url?: string;
+	disabled?: boolean;
 }
 
-const CustomButton = ({ title, action, url }: CustomButtonProps) => {
-  const handleClick = () => {
-    if (url) {
-      window.open(url, '_blank');
-    }
-    if (action) {
-      action();
-    }
-  };
+const CustomButton = ({ title, action, url, disabled }: CustomButtonProps) => {
+	const handleClick = () => {
+		if (url) {
+			window.open(url, "_blank");
+		}
+		if (action) {
+			action();
+		}
+	};
 
-  return (
-    <button
-      onClick={handleClick}
-      className="py-3 px-7 font-semibold text-base bg-main text-white rounded-lg transition-all duration-200 hover:opacity-90"
-    >
-      {title}
-    </button>
-  );
+	return (
+		<button
+			onClick={handleClick}
+			className={`py-3 px-7 font-semibold text-base bg-main text-white rounded-lg transition-all duration-200 hover:opacity-90 ${
+				disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
+			}`}
+			disabled={disabled}
+		>
+			{title}
+		</button>
+	);
 };
 
 export default CustomButton;
